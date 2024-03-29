feaf05da3a360abf49040bfdfbe9e72026708fa8
QUAD <44305177+maponarooo@users.noreply.github.com>
2024-03-29 16:43:13 +0900

    Update mavros_circle.py
    Debuging.

--- mavros_circle.py
+++ mavros_circle.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from mavros_msgs.msg import State
-from mavros_msgs.srv import CommandBool, SetMode
+from mavros_msgs.srv import CommandBool, SetMode, SetModeRequest, CommandBoolRequest
 from geometry_msgs.msg import PoseStamped, Twist
 from math import pow, sqrt
 import math
@@ -22,46 +22,55 @@ def position_cb(msg):
 def calculate_distance(x1, y1, x2, y2):
     return sqrt(pow((x2-x1),2) + pow((y2-y1),2))
 
-rospy.init_node('offboard_node', anonymous=True)
-rate = rospy.Rate(20.0)
+if __name__ == "__main__":
+    rospy.init_node('offboard_node')
+    rate = rospy.Rate(20.0)
 
-state_sub = rospy.Subscriber("/mavros/state", State, state_cb)
-local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
-local_pos_sub = rospy.Subscriber('mavros/local_position/pose', PoseStamped, position_cb)
+    state_sub = rospy.Subscriber("/mavros/state", State, state_cb)
+    local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
+    local_pos_sub = rospy.Subscriber('mavros/local_position/local', PoseStamped, position_cb)
+    rospy.wait_for_service("/mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+    rospy.wait_for_service("/mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
 
-arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
-set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
+    while (not rospy.is_shutdown() and not current_state.connected):
+        rate.sleep()
 
-while not rospy.is_shutdown() and not current_state.connected:
-    rate.sleep()
+    pose.pose.position.x = 0.0
+    pose.pose.position.y = 0.0
+    pose.pose.position.z = 2.0
 
-pose.pose.position.x = 0
-pose.pose.position.y = 0
-pose.pose.position.z = 2
+    for i in range(100):
+        if(rospy.is_shutdown()):
+            break
+            
+        local_pos_pub.publish(pose)
+        rate.sleep()
 
-for i in range(100):
-    local_pos_pub.publish(pose)
-    rate.sleep()
+    offb_set_mode = SetModeRequest()
+    offb_set_mode.custom_mode = 'OFFBOARD'
+    
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
 
-offb_set_mode = set_mode_client(0, "OFFBOARD")
-arm_cmd = arming_client(True)
+    if (set_mode_client.call(offb_set_mode).mode_sent == True):
+        rospy.loginfo("Offboard enabled")
 
-if offb_set_mode and offb_set_mode.success:
-    rospy.loginfo("Offboard enabled")
-if arm_cmd and arm_cmd.success:
-    rospy.loginfo("Vehicle armed")
+    if (arming_client.call(arm_cmd).success == True):
+        rospy.loginfo("Vehicle armed")
 
-rospy.loginfo("Taking off...")
+    rospy.loginfo("Taking off...")
 
-while not rospy.is_shutdown():
-    distance = calculate_distance(pose.pose.position.x, pose.pose.position.y, 0, 0)
-    if distance > radius:
-        pose.pose.position.x = pose.pose.position.x/radius
-        pose.pose.position.y = pose.pose.position.y/radius
-    else:
-        theta = math.atan2(pose.pose.position.y, pose.pose.position.x) + 0.1
-        pose.pose.position.x = radius * math.cos(theta)
-        pose.pose.position.y = radius * math.sin(theta)
+    while not rospy.is_shutdown():
+        distance = calculate_distance(pose.pose.position.x, pose.pose.position.y, 0, 0)
+        if distance > radius:
+            pose.pose.position.x = pose.pose.position.x/radius
+            pose.pose.position.y = pose.pose.position.y/radius
+        else:
+            theta = math.atan2(pose.pose.position.y, pose.pose.position.x) + 0.1
+            pose.pose.position.x = radius * math.cos(theta)
+            pose.pose.position.y = radius * math.sin(theta)
 
-    local_pos_pub.publish(pose)
-    rate.sleep()
+        local_pos_pub.publish(pose)
+        rate.sleep()
